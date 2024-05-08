cf0674d6746b2b9ecfb2b0e0d73b79647acb26f8
kaushal jalal <abhisheksingh2239@gmail.com>
2024-05-08 17:22:55 +0530

    image issue resolved

--- app/page.js
+++ app/page.js
@@ -1,11 +1,10 @@
 import Image from "next/image";
-import construction from "../public/construction.jpg";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen w-screen overflow-x-hidden flex-col items-center p-6 ">
       <div className="text-2xl mb-40 font-bold">Abhishek Codes</div>
-      <Image src={construction} width={500} />
+      <Image src="/construction.jpg" width={500} height={500} />
       <div className="text-lg mt-[-30px] text-gray-600 font-semibold">
         Site is under Construction!
       </div>
