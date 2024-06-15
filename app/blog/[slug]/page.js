e0f0f0226a39dd973ce43eeb62cfbc3d2e10ae44
kaushal jalal <abhisheksingh2239@gmail.com>
2024-06-15 19:25:11 +0530

    breadcrumb added to blog detail page

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Link from "next/link";
 import { apiUrl } from "@/utils/config";
 import { transformDate } from "@/utils/miscFunctions";
 
@@ -81,6 +82,75 @@ const page = async (props) => {
 
   return (
     <div className="container mt-16 mx-auto px-4">
+      <nav className="flex" aria-label="Breadcrumb">
+        <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+          <li className="inline-flex items-center">
+            <Link
+              href="/"
+              className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600"
+            >
+              <svg
+                className="w-3 h-3 me-2.5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+              </svg>
+              Home
+            </Link>
+          </li>
+          <li>
+            <div className="flex items-center">
+              <svg
+                className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m1 9 4-4-4-4"
+                />
+              </svg>
+              <Link
+                href="/blog"
+                className="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2"
+              >
+                Blog
+              </Link>
+            </div>
+          </li>
+          <li aria-current="page">
+            <div className="flex items-center">
+              <svg
+                className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m1 9 4-4-4-4"
+                />
+              </svg>
+              <span className="ms-1 text-sm font-medium text-gray-500 md:ms-2">
+                {props.params.slug}
+              </span>
+            </div>
+          </li>
+        </ol>
+      </nav>
+
       <header className="my-8 border-2 border-blue-200 p-4 rounded">
         <h1 className="text-4xl font-bold">{blog.title}</h1>
         <p className="text-gray-600">
