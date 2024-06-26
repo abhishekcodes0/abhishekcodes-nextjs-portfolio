3b6a2d7aefbd80906d12b5069f87f6152d9797f5
kaushal jalal <abhisheksingh2239@gmail.com>
2024-06-26 11:24:53 +0530

    added Gtag

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import NavBar from "./components/NavBar";
 import { bebas, montserrat } from "./fonts.js";
+import Script from "next/script";
 
 export const metadata = {
   title: "Abhishek Codes",
@@ -11,6 +12,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-TN30DD9SK8"
+        />
+        <Script>
+          {` window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-TN30DD9SK8');`}
+        </Script>
+      </head>
       <body className={montserrat.className}>
         <NavBar />
         {children}
