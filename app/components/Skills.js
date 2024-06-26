accbc705c21a912cc8aebc232adecd73f8b08068
kaushal jalal <abhisheksingh2239@gmail.com>
2024-06-26 15:00:31 +0530

    styling changes

--- app/components/Skills.js
+++ app/components/Skills.js
@@ -6,12 +6,66 @@ const Skills = () => {
     <section className="w-screen md:h-screen bg-white flex flex-col items-center">
       <div
         className={
-          "text-center text-6xl font-bold mt-12 mb-12" + " " + bebas.className
+          "text-center text-6xl font-bold mt-12 mb-0 md:mb-12" +
+          " " +
+          bebas.className
         }
       >
         Skills
       </div>
-      <div className="w-5/6 my-20 md:my-auto">
+      <div className="w-5/6 mt-10 mb-32 md:my-auto">
+        <div className="skills" style={{ "--time": "40s" }}>
+          <div>
+            <span>HTML</span>
+            <span>CSS</span>
+            <span>JavaScript</span>
+            <span>ReactJS</span>
+            <span>Figma</span>
+            <span>MongoDB</span>
+            <span>NextJs</span>
+            <span>Figma</span>
+            <span>Express</span>
+            <span>Node JS</span>
+          </div>
+          <div>
+            <span>HTML</span>
+            <span>CSS</span>
+            <span>JavaScript</span>
+            <span>ReactJS</span>
+            <span>Figma</span>
+            <span>MongoDB</span>
+            <span>NextJs</span>
+            <span>Figma</span>
+            <span>Express</span>
+            <span>Node JS</span>
+          </div>
+        </div>
+        <div className="skills" style={{ "--time": "15s" }}>
+          <div>
+            <span>HTML</span>
+            <span>CSS</span>
+            <span>JavaScript</span>
+            <span>ReactJS</span>
+            <span>Figma</span>
+            <span>MongoDB</span>
+            <span>NextJs</span>
+            <span>Figma</span>
+            <span>Express</span>
+            <span>Node JS</span>
+          </div>
+          <div>
+            <span>HTML</span>
+            <span>CSS</span>
+            <span>JavaScript</span>
+            <span>ReactJS</span>
+            <span>Figma</span>
+            <span>MongoDB</span>
+            <span>NextJs</span>
+            <span>Figma</span>
+            <span>Express</span>
+            <span>Node JS</span>
+          </div>
+        </div>
         <div className="skills" style={{ "--time": "20s" }}>
           <div>
             <span>HTML</span>
